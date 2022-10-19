8f011703fe569615f1739e13ffa2b2c8922ceeaf
martoast <alexmartos96@gmail.com>
2022-10-19 15:56:28 -0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,98 @@
+<template>
+  <div id="app">
+    <dashboard :id="'dashExample'">
+      <dash-layout ref="dashLayout" v-for="layout in dlayouts" v-bind="layout" :debug="true" :key="layout.breakpoint">
+        <dash-item v-for="item in layout.items" v-bind.sync="item" v-on:moveEnd="moveEnd" :key="item.id">
+          <div class="content"></div>
+        </dash-item>
+      </dash-layout>
+    </dashboard>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      dlayouts: [
+        {
+          breakpoint: "xl",
+          numberOfCols: 12,
+          items: [
+            { id: "1", x: 0, y: 0, width: 1, height: 1 },
+            { id: "2", x: 1, y: 0, width: 2, height: 1 },
+          ]
+        },
+        {
+          breakpoint: "lg",
+          breakpointWidth: 1200,
+          numberOfCols: 10,
+          items: [
+            { id: "1", x: 0, y: 0, width: 1, height: 1 },
+            { id: "2", x: 1, y: 0, width: 2, height: 1 },
+          ]
+        },
+        {
+          breakpoint: "md",
+          breakpointWidth: 996,
+          numberOfCols: 8,
+          items: [
+            { id: "1", x: 0, y: 0, width: 1, height: 1 },
+            { id: "2", x: 1, y: 0, width: 2, height: 1 },
+          ]
+        },
+        {
+          breakpoint: "sm",
+          breakpointWidth: 768,
+          numberOfCols: 4,
+          items: [
+            { id: "1", x: 0, y: 0, width: 1, height: 1 },
+            { id: "2", x: 1, y: 0, width: 2, height: 1 },
+          ]
+        },
+        {
+          breakpoint: "xs",
+          breakpointWidth: 480,
+          numberOfCols: 2,
+          items: [
+            { id: "1", x: 0, y: 0, width: 1, height: 1 },
+            { id: "2", x: 1, y: 0, width: 1, height: 1 },
+          ]
+        },
+        {
+          breakpoint: "xxs",
+          breakpointWidth: 0,
+          numberOfCols: 1,
+          items: [
+            {
+              id: "1",
+              x: 0,
+              y: 0,
+              width: 1,
+              height: 1
+            },
+            { id: "2", x: 0, y: 1, width: 1, height: 1 }
+          ]
+        }
+      ]
+    };
+  },
+  methods: {
+    moveEnd() {
+      this.$nextTick(() => {
+        this.$refs.dashLayout[0].placeholderY = this.maxY + this.maxH;
+      });
+    },
+  }
+};
+</script>
+
+<style>
+.content {
+  height: 100%;
+  width: 100%;
+  border: 2px solid #42b983;
+  border-radius: 5px;
+}
+</style>
---
+++ plugins/vue-responsive-dash.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import { Dashboard, DashLayout, DashItem } from "vue-responsive-dash";
+
+Vue.component("dashboard", Dashboard);
+Vue.component("dash-layout", DashLayout);
+Vue.component("dash-item", DashItem);
+
